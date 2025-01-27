e15ccdb6579b7dcc2986c53acbd10b5adbf5b270
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2025-01-27 06:48:37 +0300

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 '''A module with tools for request caching and tracking.
 '''
-
 import redis
 import requests
 from functools import wraps
@@ -14,30 +13,20 @@ redis_store = redis.Redis()
 
 
 def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data and tracks the access count.
+    '''Caches the output of fetched data.
     '''
     @wraps(method)
-    def invoker(url: str) -> str:
-        '''The wrapper function for caching the output and incrementing access count.
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
         '''
-        # Increment the access count for this URL
         redis_store.incr(f'count:{url}')
-        
-        # Try to fetch the cached result
         result = redis_store.get(f'result:{url}')
-        
-        # If result is cached, return it
         if result:
             return result.decode('utf-8')
-        
-        # If no cached result, fetch it using the method
         result = method(url)
-        
-        # Cache the result with an expiration time of 10 seconds
+        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
-        
         return result
-
     return invoker
 
 
@@ -46,6 +35,4 @@ def get_page(url: str) -> str:
     '''Returns the content of a URL after caching the request's response,
     and tracking the request.
     '''
-    response = requests.get(url)
-    return response.text
-
+    return requests.get(url).text
