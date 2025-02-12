4fad1263dbf16a754f7fed5b1df16a6d54e50db0
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2025-02-12 07:03:46 +0300

    corrections

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
 from functools import wraps
 
-# Set up the Redis client
+
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
+'''The module-level Redis instance.
+'''
 
 
 def cache_page(func):
-    """
+    '''
     A decorator to cache the page content and track access count in Redis.
 
     Args:
@@ -16,10 +20,10 @@ def cache_page(func):
 
     Returns:
         function: The wrapped function with caching and access tracking.
-    """
+    '''
     @wraps(func)
     def wrapper(url: str) -> str:
-        """
+        '''
         A wrapper function to handle caching and access count in Redis.
 
         Args:
@@ -27,27 +31,21 @@ def cache_page(func):
 
         Returns:
             str: The content of the page.
-        """
-        # Check if page content is already cached
+        '''
         cached_content = r.get(url)
         if cached_content:
-            # Increment the access count
             count_key = f"count:{url}"
             r.incr(count_key)
             return cached_content.decode('utf-8')
 
-        # If not cached, fetch the content
         content = func(url)
 
-        # Cache the content with an expiration time of 10 seconds
         r.setex(url, 10, content)
 
-        # Initialize access count if not set
         count_key = f"count:{url}"
         if not r.exists(count_key):
-            r.set(count_key, 0)  # Initialize the count to 0
+            r.set(count_key, 0)
 
-        # Increment the access count
         r.incr(count_key)
 
         return content
@@ -57,7 +55,7 @@ def cache_page(func):
 
 @cache_page
 def get_page(url: str) -> str:
-    """
+    '''
     Fetches the HTML content of a URL.
 
     Args:
@@ -65,6 +63,6 @@ def get_page(url: str) -> str:
 
     Returns:
         str: The HTML content of the page.
-    """
+    '''
     response = requests.get(url)
     return response.text
